9af9946bc066d058adb3719abba3843bea086a88
lights0123 <developer@lights0123.com>
2020-09-22 16:23:39 -0400

    Fix segfault when trying to open already-opened device

--- libnspire/src/error.rs
+++ libnspire/src/error.rs
@@ -9,7 +9,14 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 /// A libnspire error.
 #[derive(Display, Error, Debug)]
+#[non_exhaustive]
 pub enum Error {
+    /// Input/output error
+    Io,
+    /// Access denied (insufficient permissions)
+    Access,
+    /// Operation not supported or unimplemented on this platform
+    NotSupported,
     /// Timeout
     Timeout,
     /// Out of memory
@@ -42,6 +49,7 @@ pub enum Error {
 
 pub(crate) fn err(code: c_int) -> Result<()> {
     use libnspire_sys::*;
+    use libusb1_sys::constants::*;
     #[forbid(unreachable_patterns)]
     match code as c_uint {
         NSPIRE_ERR_SUCCESS => Ok(()),
@@ -55,6 +63,20 @@ pub(crate) fn err(code: c_int) -> Result<()> {
         NSPIRE_ERR_INVALID => Err(Error::Invalid),
         NSPIRE_ERR_EXISTS => Err(Error::Exists),
         NSPIRE_ERR_NONEXIST => Err(Error::DoesNotExist),
-        _ => Err(Error::Unknown),
+        _ => match code {
+            LIBUSB_ERROR_IO => Err(Error::Io),
+            LIBUSB_ERROR_INVALID_PARAM => Err(Error::Invalid),
+            LIBUSB_ERROR_ACCESS => Err(Error::Access),
+            LIBUSB_ERROR_NO_DEVICE => Err(Error::NoDevice),
+            LIBUSB_ERROR_NOT_FOUND => Err(Error::NoDevice),
+            LIBUSB_ERROR_BUSY => Err(Error::Busy),
+            LIBUSB_ERROR_TIMEOUT => Err(Error::Timeout),
+            LIBUSB_ERROR_OVERFLOW => Err(Error::LibUsb),
+            LIBUSB_ERROR_PIPE => Err(Error::LibUsb),
+            LIBUSB_ERROR_INTERRUPTED => Err(Error::LibUsb),
+            LIBUSB_ERROR_NO_MEM => Err(Error::OutOfMemory),
+            LIBUSB_ERROR_NOT_SUPPORTED => Err(Error::NotSupported),
+            _ => Err(Error::Unknown),
+        },
     }
 }
